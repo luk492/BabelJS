472a68a85f05976a539163ca153fd79e58dbad52
Luka  KAJTES <luka.kajtes.prestataire@prima-solutions.com>
2018-05-24 15:03:30 +0200

    Init

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+})
