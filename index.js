7af8de5474ee3886f95ff2324ce494fc57ca28ab
Luka  KAJTES <luka.kajtes.prestataire@prima-solutions.com>
2018-06-22 16:53:15 +0200

    trigger travis

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-// Do stuff
-const f = () => {
-    
-}
+var wait1000 =  ()=> new Promise((resolve, reject)=> {
+    setTimeout(resolve, 1000)
+})
+
+wait1000()
+ .then(function() {
+    console.log('Yay!')
+    return wait1000()
+  })
+  .then(function() {
+    console.log('Wheeyee!')
+  })
